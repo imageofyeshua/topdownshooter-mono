8a9f99ba051ad70abd7d66c720923caea33fc01f
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-18 16:47:03 +0900

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopDownShooter;
+
+public class Main : Game
+{
+  private GraphicsDeviceManager _graphics;
+  private SpriteBatch _spriteBatch;
+
+  public Main()
+  {
+    _graphics = new GraphicsDeviceManager(this);
+    Content.RootDirectory = "Content";
+    IsMouseVisible = true;
+  }
+
+  protected override void Initialize()
+  {
+    // TODO: Add your initialization logic here
+
+    base.Initialize();
+  }
+
+  protected override void LoadContent()
+  {
+    Globals.content = this.Content;
+    Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+    // TODO: use this.Content to load your game content here
+  }
+
+  protected override void UnloadContent()
+  {
+    // TODO: Unload any non ContentManager content here 
+  }
+
+  protected override void Update(GameTime gameTime)
+  {
+    if (
+        GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+        || Keyboard.GetState().IsKeyDown(Keys.Escape)
+    )
+      Exit();
+
+    // TODO: Add your update logic here
+
+    base.Update(gameTime);
+  }
+
+  protected override void Draw(GameTime gameTime)
+  {
+    GraphicsDevice.Clear(Color.CornflowerBlue);
+
+    // TODO: Add your drawing code here
+
+    Globals.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+
+
+    Globals.spriteBatch.End();
+
+    base.Draw(gameTime);
+  }
+}
---
+++ Source/Engine/Globals.cs
@@ -0,0 +1,22 @@
+#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace TopDownShooter
+{
+  class Globals
+  {
+    public static ContentManager content;
+    public static SpriteBatch spriteBatch;
+  }
+}
