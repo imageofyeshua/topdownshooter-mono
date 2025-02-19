38362282130b2b9c08bcdbb9a9ce94f8bf12e6d6
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-19 14:02:07 +0900

    Rotation toward Cursor

--- Source/GamePlay/World/Hero.cs
+++ Source/GamePlay/World/Hero.cs
@@ -2,9 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -17,33 +17,43 @@ namespace TopDownShooter;
 
 public class Hero : Basic2d
 {
-    public Hero(string Path, Vector2 Pos, Vector2 Dims) : base(Path, Pos, Dims)
+  public float speed;
+
+  public Hero(string Path, Vector2 Pos, Vector2 Dims)
+      : base(Path, Pos, Dims)
+  {
+    speed = 2.0f;
+  }
+
+  public override void Update()
+  {
+    if (Globals.keyboard.GetPress("A"))
     {
+      pos = new Vector2(pos.X - speed, pos.Y);
+    }
+    if (Globals.keyboard.GetPress("D"))
+    {
+      pos = new Vector2(pos.X + speed, pos.Y);
+    }
+    if (Globals.keyboard.GetPress("W"))
+    {
+      pos = new Vector2(pos.X, pos.Y - speed);
+    }
+    if (Globals.keyboard.GetPress("S"))
+    {
+      pos = new Vector2(pos.X, pos.Y + speed);
     }
 
-    public override void Update()
-    {
-      if (Globals.keyboard.GetPress("A"))
-      {
-        pos = new Vector2(pos.X - 1, pos.Y);
-      }
-      if (Globals.keyboard.GetPress("D"))
-      {
-        pos = new Vector2(pos.X + 1, pos.Y);
-      }
-      if (Globals.keyboard.GetPress("W"))
-      {
-        pos = new Vector2(pos.X, pos.Y - 1);
-      }
-      if (Globals.keyboard.GetPress("S"))
-      {
-        pos = new Vector2(pos.X, pos.Y + 1);
-      }
-    }
+    rot = Globals.RotateTowards(
+        pos,
+        new Vector2(Globals.mouse.newMousePos.X, Globals.mouse.newMousePos.Y)
+    );
 
-    public override void Draw(Vector2 Offset)
-    {
-        base.Draw(Offset);
-    }
+    base.Update();
+  }
 
+  public override void Draw(Vector2 Offset)
+  {
+    base.Draw(Offset);
+  }
 }
